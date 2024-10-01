a308d08ba8fc01c5cc496974414b5c4eb22536cd
Julio Gema <julid@accelbyte.net>
2024-10-01 13:47:22 +0700

    learn more about primitives

--- 02-primitives/src/main.rs
+++ 02-primitives/src/main.rs
@@ -1,3 +1,16 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    let xs: [i32;5] = [1,2,3,4,5];
+
+    println!("First element of the array: {}", xs[0]);
+    println!("Length of the array: {}", xs.len());
+    println!("Array occupies {} bytes", mem::size_of_val(&xs));
+
+    for i in 0..xs.len() + 1{
+        match xs.get(i) {
+            Some(xval) => println!("{}: {}", i, xval),
+            None => println!("Slow down! {} is too far",i),
+        }
+    }
 }
